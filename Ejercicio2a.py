390c636e5517db73c00a9930d069b5bf1db79774
Anthony <acahuib@unsa.edu.pe>
2024-05-25 10:33:01 -0500

    Segun el ejrcicio no pide aun el SQUARE, se elimina eso

--- Ejercicio2a.py
+++ Ejercicio2a.py
@@ -3,15 +3,11 @@ from interpreter import draw
 from pieces import SQUARE, KNIGHT  # Importa las imágenes de las fichas desde pieces.py
 
 # Define las imágenes de las fichas de ajedrez
-# SQUARE y KNIGHT se importan de pieces.py
-
-# Crea objetos Picture para las casillas y las fichas
-casilla_azul = Picture(SQUARE)
 caballo_negro = Picture(KNIGHT)
 caballo_blanco = Picture(KNIGHT)
 
 # Combina las casillas y los caballos en una fila
-fila1 = casilla_azul.join(casilla_azul).join(caballo_negro).join(caballo_blanco)
+fila1 = caballo_negro.join(caballo_blanco)
 fila2 = fila1.verticalRepeat(2)  # Repite la fila dos veces para formar un cuadrado
 
 # Dibuja las cuatro casillas con los caballos
