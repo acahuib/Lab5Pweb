ca6e78a41dfc76fff666271f4399dc5052aa8f1a
Anthony <acahuib@unsa.edu.pe>
2024-05-25 20:31:30 -0500

    Se arregla las posiciones de las fichas

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -2,15 +2,26 @@ from interpreter import draw
 from chessPictures import *
 from picture import Picture
 
-# Crear una fila de 8 fichas blancas alternando entre bishop, king, knight, pawn, queen, rock y square
-fila_blanca = bishop.join(king).join(knight).join(pawn).join(queen).join(rock).join(square).horizontalRepeat(8)
+# Crear cuadros claro y oscuro
+cuadro_claro = square
+cuadro_oscuro = square.negative()
 
-# Crear una fila de 8 fichas negras alternando entre bishop, king, knight, pawn, queen, rock y square
-fila_negra = bishop.negative().join(king.negative()).join(knight.negative()).join(pawn.negative()).join(queen.negative()).join(rock.negative()).join(square.negative()).horizontalRepeat(8)
+# Crear una fila de cuadros intercalados
+fila_cuadros_1 = cuadro_claro.join(cuadro_oscuro).horizontalRepeat(4)
+fila_cuadros_2 = cuadro_oscuro.join(cuadro_claro).horizontalRepeat(4)
 
-# Crear la cuadrícula de ajedrez de 8x8 con fichas alternando en el patrón estándar
-cuadricula_ajedrez = fila_blanca.under(fila_negra).verticalRepeat(4)
+# Crear el tablero completo intercalando las filas de cuadros
+tablero = fila_cuadros_1.under(fila_cuadros_2).verticalRepeat(4)
 
-# Dibujar la cuadrícula de ajedrez
-draw(cuadricula_ajedrez)
+# Colocar las piezas en sus posiciones iniciales
+fila_piezas_negras = rook.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rook)
+fila_peones_negros = pawn.horizontalRepeat(8)
+fila_peones_blancos = fila_peones_negros.verticalMirror()
+fila_piezas_blancas = fila_piezas_negras.verticalMirror()
+
+# Colocar las filas de piezas sobre el tablero
+tablero_con_piezas = fila_piezas_negras.under(fila_peones_negros).under(tablero).under(fila_peones_blancos).under(fila_piezas_blancas)
+
+# Dibujar el tablero con las piezas
+draw(tablero_con_piezas)
 
